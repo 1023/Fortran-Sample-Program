e4492efaff6a39f082603da28471b0544b90d1d5
onodera <ga19981023ku@gmail.com>
2020-04-19 23:41:37 +0900

    [add] 2章　式の計算

--- HW2.5.f95
+++ HW2.5.f95
@@ -1,11 +1,13 @@
-program EX21
+program HW25
     !積の計算
     pai=3.141592
+    rho=7.8
     write(6,*)'r1,r2,h >>' !6はディスプレイへの出力
     read(5,*) r1,r2,h !5はキーボードでの入力
     write(6,*)'seki no keisan'
     write(6,*) 'r1=' ,r1,'r2=',r2,'h=',h
     v=pai*(r2**2-r1**2)*h/4.0 !**は累乗を意味する
     s=pai*((r2**2-r1**2)/2.0+(r1+r2)*h)
-    write(6,*)'v=',v,'s=',s
+    w=v*rho
+    write(6,*)'v=',v,'s=', s ,'w',w
 End
---
+++ HW2.6.f95
@@ -0,0 +1,8 @@
+program EX21
+    !cm,kgのインチ,ポンドへの変換
+    write(6,*)'kg,cm >>' 
+    read(5,*) kg,cm
+    pound=kg/0.4539
+    inch=cm/2.54
+    write(6,*)'pound=',pound,'inch=', inch
+End

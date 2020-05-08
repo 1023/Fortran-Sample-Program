1c84c484f1253fca07fb3c0586437c77c04fefb7
onodera <ga19981023ku@gmail.com>
2020-05-09 00:01:54 +0900

    [remove]

--- EX93.f95
+++
@@ -1,7 +0,0 @@
-program EX93
-    !AからZまでの内部コードの印刷
-    ic=ichar('A')
-    write(6,"(6('    ',a1,':',i2))")(char(ic+i-1),ic+i-1,i=1,26)
-    is=ichar('a')
-    write(6,"(6('    ',a1,':',i3))")(char(is+i-1),is+i-1,i=1,26)
-end program EX93
